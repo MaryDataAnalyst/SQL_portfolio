f16beb168f9777541b5eb18ed3d1c212e59d95d5
Mariia Shvaia <152871711+MaryDataAnalyst@users.noreply.github.com>
2024-05-17 22:48:47 +0200

    Delete Monthly Percentage Difference - Amazon.sql

--- Monthly Percentage Difference - Amazon.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-date_format(created_at, '%Y-%m') as date,
-ROUND((sum(value) - lag(sum(value)) OVER())/lag(sum(value)) over() * 100, 2) as diff
-FROM sf_transactions
-GROUP BY date
-ORDER BY date
